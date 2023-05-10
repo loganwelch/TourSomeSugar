6d2024d00f55daffb29c12ff2cc80be39df83a36
Logan Welch <loganewelch@gmail.com>
2023-05-10 10:38:42 -0500

    completed the event listener for the bands module

--- scripts/Bands.js
+++ scripts/Bands.js
@@ -1,16 +1,69 @@
-import { getBands } from "./database.js"
+import { getBands, getVenues, getBookings } from "./database.js"
 
-const bandList = getBands()
+const venuesList = getVenues()
+const bookingsList = getBookings()
+const bandsList = getBands()
 
-//add event listener to display window alert showing all the venues the clicked band is playing at
+// Function whose responsibility is to find the venue for a band
+const findVenue = (booking, allVenues) => {
+    let bookingVenue = null
+
+    for (const venue of allVenues) {
+        if (venue.id === booking.venueId) {
+            bookingVenue = venue
+        }
+    }
+
+    return bookingVenue
+}
+
+const findMessage = (bandId) => {
+    //let message = ``
+    const matchingVenues = []
+
+
+    for (const booking of bookingsList) {
+        if (booking.bandId === bandId) {
+            const venue = findVenue(booking, venuesList)
+            //const venue = findVenue(booking, venuesList)
+            matchingVenues.push(venue.name)
+            //let venueBands = band.join(' , ')
+
+            //return message = `${venueBands} is playing ${venue.name}`
+        }
+    }
+    if (matchingVenues.length === 0) {
+        return 'This band is not playing at any venues.'
+    } else {
+        return `This band is playing at the following venues: ${matchingVenues.join(' ,')}.`
+    }
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        // Was a venue list item clicked?
+        if (itemClicked.dataset.type === "band") {
+            const bandId = parseInt(itemClicked.dataset.id);
+
+            for (const band of bandsList) {
+                if (band.id === bandId) {
+                    const alert = findMessage(bandId)
+                    window.alert(alert)
+                }
+            }
+        }
+    }
+)
 
 
 
 export const Bands = () => {
     let html = "<ul>"
 
-    for (const band of bandList) {
-        html += `<li>${band.name}</li>`
+    for (const band of bandsList) {
+        html += `<li data-type="band" data-id="${band.id}">${band.name}</li>`
     }
 
 
